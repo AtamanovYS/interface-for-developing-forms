ba3034c9f9ae9faeeb285d7497f32c8c4a415ed9
AtamanovYS <75320713+AtamanovYS@users.noreply.github.com>
2021-11-19 04:22:24 +1000

    Update multiple_IN.sql

--- multiple_IN.sql
+++ multiple_IN.sql
@@ -1,4 +1,4 @@
--- Аналог оператора множественного оператора В (IN) из 1С: [SELECT * FROM Т WHERE (Т.a, Т.b) IN (SELECT Т'.a, Т'.b FROM Т')]
+-- Аналог множественного оператора В (IN) из 1С: [SELECT * FROM Т WHERE (Т.a, Т.b) IN (SELECT Т'.a, Т'.b FROM Т')]
 
 SELECT * FROM T WHERE EXISTS (SELECT 1 FROM
                               T' WHERE T.a = T'.a AND
