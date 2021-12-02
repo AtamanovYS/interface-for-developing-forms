da0bdf0129eee9757df44ae9a53d4861e3c923e0
AtamanovYS <75320713+AtamanovYS@users.noreply.github.com>
2021-12-03 02:15:47 +1000

    Delete multiple_IN.sql

--- multiple_IN.sql
+++
@@ -1,5 +0,0 @@
--- Аналог множественного оператора В (IN) из 1С: [SELECT * FROM Т WHERE (Т.a, Т.b) IN (SELECT Т'.a, Т'.b FROM Т')]
-
-SELECT * FROM T WHERE EXISTS (SELECT 1 FROM
-                              T' WHERE T.a = T'.a AND
-                                       T.b = T'.b)
